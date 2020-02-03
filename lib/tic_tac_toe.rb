483761df6f80998b71beff1b34be24507c4fb791
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-03 19:37:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,7 +66,6 @@ class TicTacToe
           return win_combo
         elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
           return win_combo
-          nil
         end
           false
         
