ccde417d83e52b1dbad339569570002209542580
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-03 19:39:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,7 +87,7 @@ class TicTacToe
     def winner?
       WIN_COMBINATIONS.detect do |win_combo|
         if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
-          return "x"
+          return "X"
         elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
           return "O"
         else 
